33de5e4f58f18774ece4696f1f1c06c411286b22
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-12 03:14:29 +0530

    Add files via upload

---
+++ Turtle.py
@@ -0,0 +1,25 @@
+#####Turtle Intro######
+
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+screen = Screen()
+turtle.shape("turtle")
+turtle.color("red")
+
+######## Challenge 1 - Draw a Square ############
+
+def square():
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    
+square()
+
+screen = Screen()
+screen.exitonclick()
